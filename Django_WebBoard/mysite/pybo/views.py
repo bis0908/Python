1a63f284bd1b1b1bc0b9c519f49ee030841bfde1
bis0908 <kamirof@gmail.com>
2021-08-22 01:39:15 +0900

    2021.08.22
    Django WebBoard Project

--- Django_WebBoard/mysite/pybo/views.py
+++ Django_WebBoard/mysite/pybo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 # Create your views here.
 # from django.http import HttpResponse
 
@@ -19,3 +21,35 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    # pybo 답변 등록
+    question = get_object_or_404(Question, pk = question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)  # False: DB commit X (임시 저장)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id = question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    # pybo 질문 등록
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)  # False: DB commit X (임시 저장)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form }
+    return render(request, 'pybo/question_from.html', context)
+
+
